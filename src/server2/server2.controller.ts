aea861c0decd12f7db6defaf0fbcbe1fe2b1a1cf
davidpaz06 <davidalbertopaz@gmail.com>
2025-06-09 10:04:43 -0400

    Remove unused Server2 and Server3 modules, controllers, and services

--- src/server2/server2.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { Server2Service } from './server2.service';
-
-@Controller('server2')
-export class Server2Controller {
-  constructor(private readonly server2Service: Server2Service) {}
-}
--- src/server2/server2.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Server2Service } from './server2.service';
-import { Server2Controller } from './server2.controller';
-
-@Module({
-  controllers: [Server2Controller],
-  providers: [Server2Service],
-})
-export class Server2Module {}
