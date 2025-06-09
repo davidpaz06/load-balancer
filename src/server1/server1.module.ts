99646a4f11f05eac51184169d28c0ce06f566c8e
davidpaz06 <davidalbertopaz@gmail.com>
2025-06-09 10:07:24 -0400

    Remove unused Server1 module, controller, and service

--- src/server1/server1.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Server1Service } from './server1.service';
-import { Server1Controller } from './server1.controller';
-
-@Module({
-  controllers: [Server1Controller],
-  providers: [Server1Service],
-})
-export class Server1Module {}
