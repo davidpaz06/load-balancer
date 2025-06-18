6e673e8643d27c3c79f2ebc4ba09227894b4e98c
davidpaz06 <davidalbertopaz@gmail.com>
2025-06-18 09:24:04 -0400

    Remove unused server modules and AppController from AppModule for cleaner structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProxyController } from './proxy/proxy.controller';
-import { Server1Module } from './server1/server1.module';
-import { Server2Module } from './server2/server2.module';
-import { Server3Module } from './server3/server3.module';
 
 @Module({
-  imports: [Server1Module, Server2Module, Server3Module],
-  controllers: [AppController, ProxyController],
+  imports: [],
+  controllers: [ProxyController],
   providers: [AppService],
 })
 export class AppModule {}
