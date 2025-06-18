658f078817e3a363edde67e7dc45811404277bb3
davidpaz06 <davidalbertopaz@gmail.com>
2025-06-18 09:24:12 -0400

    Remove LoggerInterceptor usage and update controller path for API routing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,23 +2,8 @@ import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LoggerInterceptor } from './logger/logger.interceptor';
 
-@UseInterceptors(LoggerInterceptor)
-@Controller()
+// @UseInterceptors(LoggerInterceptor)
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  async getHello(): Promise<Record<string, unknown>> {
-    return this.appService.getHello();
-  }
-
-  @Get('multiple')
-  async getMultipleResponses(): Promise<Record<string, unknown>[]> {
-    return this.appService.getMultipleResponses();
-  }
-
-  @Get('pokemon')
-  async fetchPokemon(): Promise<Record<string, unknown>> {
-    return this.appService.fetchPokemon();
-  }
 }
